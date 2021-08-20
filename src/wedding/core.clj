d298ab956cb3570344a3fe0d679eed1302b92dfd
David Lapeš <dave.lapes@gmail.com>
2021-08-20 10:58:23 +0200

    Add default JVM exception handler

--- src/wedding/core.clj
+++ src/wedding/core.clj
@@ -8,6 +8,11 @@
   "Starts Wedding application."
   [& args]
   (timbre/info "Starting Wedding application")
+  ;;TODO: Remove
+  (Thread/setDefaultUncaughtExceptionHandler
+    (reify Thread$UncaughtExceptionHandler
+      (uncaughtException [_ thread ex]
+        (timbre/error ex "Uncaught exception on" (.getName thread)))))
   (start (system/make-system))
   (timbre/info "Started Wedding application")
   :ok)
