0a26a537767208096fd46ba051d7ff01043a5887
David Lapeš <dave.lapes@gmail.com>
2021-08-23 15:52:08 +0200

    Fix compile issues due to requiring unexisting namespace

--- src/wedding/api/schema/table.clj
+++ src/wedding/api/schema/table.clj
@@ -1,6 +1,5 @@
 (ns wedding.api.schema.table
-  (:require [schema.core :as s]
-            [wedding.api.schema.address :refer [Address]]))
+  (:require [schema.core :as s]))
 
 (s/defschema CreateTable
   {:description (s/maybe s/Str)
