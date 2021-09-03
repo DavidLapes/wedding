2118c3757b411788a5e83fa7f4d45b972516e4eb
David Lapeš <dave.lapes@gmail.com>
2021-09-03 18:10:11 +0200

    Add address to guest for RSVP

--- src/wedding/api/schema/guest.clj
+++ src/wedding/api/schema/guest.clj
@@ -2,15 +2,10 @@
   (:require [schema.core :as s]))
 
 (s/defschema SubmitRSVP
-  {:state               s/Str
-   :city                s/Str
-   :street              s/Str
-   :orientation_number  s/Str
-   :descriptive_number  s/Str
-   :postal_code         s/Str
-   :accommodation       s/Bool
-   :email               (s/maybe s/Str)
-   :phone               s/Str})
+  {:address       s/Str
+   :accommodation s/Bool
+   :email         (s/maybe s/Str)
+   :phone         s/Str})
 
 (s/defschema CreateGuest
   {:first_name                    s/Str
@@ -26,23 +21,18 @@
    (s/optional-key :greeting_name) s/Str})
 
 (s/defschema GuestOutput
-  {:id                  s/Int
-   :first_name          s/Str
-   :middle_name         (s/maybe s/Str)
-   :last_name           s/Str
-   :greeting_name       s/Str
-   :phone               (s/maybe s/Str)
-   :email               (s/maybe s/Str)
-   :state               (s/maybe s/Str)
-   :city                (s/maybe s/Str)
-   :street              (s/maybe s/Str)
-   :orientation_number  (s/maybe s/Str)
-   :descriptive_number  (s/maybe s/Str)
-   :postal_code         (s/maybe s/Str)
-   :accommodation       s/Bool
-   :rsvp_answered       s/Bool
-   :email_sent          s/Bool
-   :table_id            (s/maybe s/Int)})
+  {:id            s/Int
+   :first_name    s/Str
+   :middle_name   (s/maybe s/Str)
+   :last_name     s/Str
+   :greeting_name s/Str
+   :phone         (s/maybe s/Str)
+   :email         (s/maybe s/Str)
+   :address       (s/maybe s/Str)
+   :accommodation s/Bool
+   :rsvp_answered s/Bool
+   :email_sent    s/Bool
+   :table_id      (s/maybe s/Int)})
 
 (s/defschema GuestListOutput
   {:data  [GuestOutput]
