82aab37f0d75bd0556ddc449766397e3a5188bb9
David Lapeš <dave.lapes@gmail.com>
2022-07-17 09:42:40 +0200

    Fix creation of room using wrong schema

--- src/wedding/api/schema/room.clj
+++ src/wedding/api/schema/room.clj
@@ -6,14 +6,14 @@
    :bed_count s/Int})
 
 (s/defschema RoomOutput
-  {:id                   s/Int
-   :number               s/Str
-   :bed_count            s/Int
-   :allocated_count      s/Int
-   :allocated_guests     [{:id        s/Int
-                           :full_name s/Str
-                           :room_id   s/Int}]
-   :available_beds_count s/Int})
+  {:id                                    s/Int
+   :number                                s/Str
+   :bed_count                             s/Int
+   (s/optional-key :allocated_count)      s/Int
+   (s/optional-key :allocated_guests)     [{:id        s/Int
+                                            :full_name s/Str
+                                            :room_id   s/Int}]
+   (s/optional-key :available_beds_count) s/Int})
 
 (s/defschema UpdateRoom
   {:number    s/Str
